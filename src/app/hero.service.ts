bf4bf099455febdc3885bd71e7b112e248bef11b
Rick Van <x.rckvan@gmail.com>
2018-02-13 14:03:56 -0700

    services

---
+++ src/app/hero.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs/observable/of';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class HeroService {
+
+  constructor() { }
+
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service'
 
 @Component({
   selector: 'app-heroes',
@@ -12,12 +12,16 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero) : void {
     this.selectedHero = hero
   }
-  
-  heroes = HEROES;
 
-  constructor() { }
+  heroes : Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): Observable<Hero[]> {
+  return of(HEROES);
+  }
 }
