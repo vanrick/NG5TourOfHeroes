6607bab943cd516cd821abb62c6f5f170ecf3a3e
Rick Van <x.rckvan@gmail.com>
2018-02-21 08:42:25 -0700

    finished routing

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,12 +1,18 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
-export class InMemHeroService implements InMemoryDbService {
+export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    let heroes = [
-      { id: 1, name: 'Windstorm' },
-      { id: 2, name: 'Bombasto' },
-      { id: 3, name: 'Magneta' },
-      { id: 4, name: 'Tornado' }
+    const heroes = [
+      { id: 11, name: 'Mr. Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
     ];
     return {heroes};
   }
